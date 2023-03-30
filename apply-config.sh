e41ba40f3ae472950c12d77d7fe13b27cd0eb303
Scotte Zinn <scotte@zinn.ca>
2023-03-30 09:40:09 -0400

    Enable container update/cleanup

--- apply-config.sh
+++ apply-config.sh
@@ -54,17 +54,16 @@ if "$dry_run"; then
   compare
 else
   # Pull new container images
-  # TODO: Re-enable
-  # AVAILABLE_IMAGES=($(run show container image | awk '{ if ( NR > 1  ) { print $1 ":" $2} }'))
-  # CONFIG_IMAGES=($(sed -nr "s/set container name .* image '(.*)'/\1/p" /config/config-parts/* | uniq))
+  AVAILABLE_IMAGES=($(run show container image | awk '{ if ( NR > 1  ) { print $1 ":" $2} }'))
+  CONFIG_IMAGES=($(sed -nr "s/set container name .* image '(.*)'/\1/p" /config/config-parts/* | uniq))
 
-  # for image in "${CONFIG_IMAGES[@]}"
-  # do
-  #   if [[ ! " ${AVAILABLE_IMAGES[*]} " =~ " ${image} " ]]; then
-  #     echo "Pulling image ${image}"
-  #     run add container image "${image}"
-  #   fi
-  # done
+  for image in "${CONFIG_IMAGES[@]}"
+  do
+    if [[ ! " ${AVAILABLE_IMAGES[*]} " =~ " ${image} " ]]; then
+      echo "Pulling image ${image}"
+      run add container image "${image}"
+    fi
+  done
 
   # Commit and save
   echo "Committing and saving config"
@@ -72,20 +71,19 @@ else
   save
 
   # Clean obsolete container images
-  # TODO: Re-enable
-  # IFS=$'\n' read -rd '' -a AVAILABLE_IMAGES <<<"$(run show container image | tail -n +2)"
-  # for image in "${AVAILABLE_IMAGES[@]}"
-  # do
-  #   image_name=$(echo "${image}" | awk '{ print $1 }')
-  #   image_tag=$(echo "${image}" | awk '{ print $2 }')
-  #   image_id=$(echo "${image}" | awk '{ print $3 }')
-  #   image_name_tag="${image_name}:${image_tag}"
+  IFS=$'\n' read -rd '' -a AVAILABLE_IMAGES <<<"$(run show container image | tail -n +2)"
+  for image in "${AVAILABLE_IMAGES[@]}"
+  do
+    image_name=$(echo "${image}" | awk '{ print $1 }')
+    image_tag=$(echo "${image}" | awk '{ print $2 }')
+    image_id=$(echo "${image}" | awk '{ print $3 }')
+    image_name_tag="${image_name}:${image_tag}"
 
-  #   if [[ ! " ${CONFIG_IMAGES[*]} " =~ " ${image_name_tag} " ]]; then
-  #     echo "Removing container ${image_name_tag}"
-  #     run delete container image "${image_id}"
-  #   fi
-  # done
+    if [[ ! " ${CONFIG_IMAGES[*]} " =~ " ${image_name_tag} " ]]; then
+      echo "Removing container ${image_name_tag}"
+      run delete container image "${image_id}"
+    fi
+  done
 fi
 
 exit
