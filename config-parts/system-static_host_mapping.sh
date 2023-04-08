8393e6fab6f40ca8d93d7f613af6141cc0d278f6
Scotte Zinn <scotte@zinn.ca>
2023-04-08 19:49:10 -0400

    Install wireguard

--- config-parts/system-static_host_mapping.sh
+++ config-parts/system-static_host_mapping.sh
@@ -6,7 +6,6 @@
 # Gateway
 set system static-host-mapping host-name gateway.zinn.tech inet 10.0.0.1
 set system static-host-mapping host-name gateway.zinn.tech alias vpn.zinn.ca
-set system static-host-mapping host-name gateway.zinn.tech alias ipv4.zinn.ca
 
 # Unifi controller and UNVR
 set system static-host-mapping host-name styx.zinn.tech inet 10.0.5.2
