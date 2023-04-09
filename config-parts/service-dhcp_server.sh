060f7d7925141234a657f3c32677230ac4280167
Scotte Zinn <scotte@zinn.ca>
2023-04-09 16:40:47 -0400

    Force NTP and longer DHCP

--- config-parts/service-dhcp_server.sh
+++ config-parts/service-dhcp_server.sh
@@ -7,7 +7,7 @@ set service dhcp-server host-decl-name
 set service dhcp-server shared-network-name LAN authoritative
 set service dhcp-server shared-network-name LAN ping-check
 set service dhcp-server shared-network-name LAN subnet 10.0.0.0/24 default-router '10.0.0.1'
-set service dhcp-server shared-network-name LAN subnet 10.0.0.0/24 lease '900'
+set service dhcp-server shared-network-name LAN subnet 10.0.0.0/24 lease '86400'
 set service dhcp-server shared-network-name LAN subnet 10.0.0.0/24 name-server '10.0.5.4'
 set service dhcp-server shared-network-name LAN subnet 10.0.0.0/24 range 0 start '10.0.0.208'
 set service dhcp-server shared-network-name LAN subnet 10.0.0.0/24 range 0 stop '10.0.0.254'
@@ -66,7 +66,7 @@ set service dhcp-server shared-network-name LAN subnet 10.0.0.0/24 static-mappin
 set service dhcp-server shared-network-name SERVICE authoritative
 set service dhcp-server shared-network-name SERVICE ping-check
 set service dhcp-server shared-network-name SERVICE subnet 10.0.4.0/24 default-router '10.0.4.1'
-set service dhcp-server shared-network-name SERVICE subnet 10.0.4.0/24 lease '900'
+set service dhcp-server shared-network-name SERVICE subnet 10.0.4.0/24 lease '86400'
 set service dhcp-server shared-network-name SERVICE subnet 10.0.4.0/24 name-server '10.0.5.4'
 set service dhcp-server shared-network-name SERVICE subnet 10.0.4.0/24 range 0 start '10.0.4.208'
 set service dhcp-server shared-network-name SERVICE subnet 10.0.4.0/24 range 0 stop '10.0.4.254'
@@ -76,7 +76,7 @@ set service dhcp-server shared-network-name SERVERS authoritative
 set service dhcp-server shared-network-name SERVERS ping-check
 set service dhcp-server shared-network-name SERVERS subnet 10.10.0.0/24 default-router '10.10.0.1'
 set service dhcp-server shared-network-name SERVERS subnet 10.10.0.0/24 domain-name 'zinn.tech'
-set service dhcp-server shared-network-name SERVERS subnet 10.10.0.0/24 lease '900'
+set service dhcp-server shared-network-name SERVERS subnet 10.10.0.0/24 lease '86400'
 set service dhcp-server shared-network-name SERVERS subnet 10.10.0.0/24 name-server '10.0.5.4'
 set service dhcp-server shared-network-name SERVERS subnet 10.10.0.0/24 range 0 start '10.10.0.200'
 set service dhcp-server shared-network-name SERVERS subnet 10.10.0.0/24 range 0 stop '10.10.0.254'
@@ -125,7 +125,7 @@ set service dhcp-server shared-network-name TRUSTED authoritative
 set service dhcp-server shared-network-name TRUSTED ping-check
 set service dhcp-server shared-network-name TRUSTED subnet 10.20.0.0/24 default-router '10.20.0.1'
 set service dhcp-server shared-network-name TRUSTED subnet 10.20.0.0/24 domain-name 'zinn.tech'
-set service dhcp-server shared-network-name TRUSTED subnet 10.20.0.0/24 lease '900'
+set service dhcp-server shared-network-name TRUSTED subnet 10.20.0.0/24 lease '86400'
 set service dhcp-server shared-network-name TRUSTED subnet 10.20.0.0/24 name-server '10.0.5.4'
 set service dhcp-server shared-network-name TRUSTED subnet 10.20.0.0/24 range 0 start '10.20.0.200'
 set service dhcp-server shared-network-name TRUSTED subnet 10.20.0.0/24 range 0 stop '10.20.0.254'
@@ -193,7 +193,7 @@ set service dhcp-server shared-network-name IOT authoritative
 set service dhcp-server shared-network-name IOT ping-check
 set service dhcp-server shared-network-name IOT subnet 192.168.1.0/24 default-router '192.168.1.1'
 set service dhcp-server shared-network-name IOT subnet 192.168.1.0/24 domain-name 'zinn.tech'
-set service dhcp-server shared-network-name IOT subnet 192.168.1.0/24 lease '900'
+set service dhcp-server shared-network-name IOT subnet 192.168.1.0/24 lease '86400'
 set service dhcp-server shared-network-name IOT subnet 192.168.1.0/24 name-server '1.1.1.1'
 set service dhcp-server shared-network-name IOT subnet 192.168.1.0/24 range 0 start '192.168.1.200'
 set service dhcp-server shared-network-name IOT subnet 192.168.1.0/24 range 0 stop '192.168.1.254'
@@ -255,7 +255,7 @@ set service dhcp-server shared-network-name IOT subnet 192.168.1.0/24 static-map
 set service dhcp-server shared-network-name GUEST authoritative
 set service dhcp-server shared-network-name GUEST ping-check
 set service dhcp-server shared-network-name GUEST subnet 192.168.2.0/24 default-router '192.168.2.1'
-set service dhcp-server shared-network-name GUEST subnet 192.168.2.0/24 lease '900'
+set service dhcp-server shared-network-name GUEST subnet 192.168.2.0/24 lease '86400'
 set service dhcp-server shared-network-name GUEST subnet 192.168.2.0/24 name-server '1.1.1.1'
 set service dhcp-server shared-network-name GUEST subnet 192.168.2.0/24 range 0 start '192.168.2.200'
 set service dhcp-server shared-network-name GUEST subnet 192.168.2.0/24 range 0 stop '192.168.2.254'
