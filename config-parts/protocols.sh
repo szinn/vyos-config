9f1e1812eb8eb114fbe1afb5fb88d9e4bbb96640
Scotte Zinn <scotte@zinn.ca>
2023-04-23 15:04:33 -0400

    Move router ASN to 65000

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -40,4 +40,4 @@ set protocols bgp neighbor 10.12.0.21 address-family ipv4-unicast
 set protocols bgp neighbor 10.12.0.21 remote-as '65002'
 
 set protocols bgp parameters router-id '10.0.0.1'
-set protocols bgp system-as '64512'
+set protocols bgp system-as '65000'
