2befb7547cb9f64db4c0c08bf9b51e2a9d95b292
Scotte Zinn <scotte@zinn.ca>
2023-04-25 19:54:46 -0400

    Simplify .tech naming

--- config-parts/system-static_host_mapping.sh
+++ config-parts/system-static_host_mapping.sh
@@ -1,21 +1,12 @@
 # Gateway and Services
 set system static-host-mapping host-name gateway.zinn.tech inet 10.0.0.1
-set system static-host-mapping host-name gateway.zinn.tech alias vpn.zinn.ca
-set system static-host-mapping host-name styx.zinn.tech inet 10.0.5.2
-set system static-host-mapping host-name styx.zinn.tech alias styx.zinn.ca
+set system static-host-mapping host-name gateway.zinn.tech alias vpn.zinn.tech
 set system static-host-mapping host-name gatus.zinn.tech inet 10.0.5.5
 set system static-host-mapping host-name onepassword-connect.zinn.tech inet 10.0.5.6
 set system static-host-mapping host-name main.zinn.tech inet 10.0.5.10
 set system static-host-mapping host-name staging.zinn.tech inet 10.0.5.11
 
-# Unifi UNVR
-set system static-host-mapping host-name zeus.zinn.tech inet 10.0.0.32
-set system static-host-mapping host-name zeus.zinn.tech alias zeus.zinn.ca
-
 # Servers
 set system static-host-mapping host-name ragnar.zinn.tech inet 10.10.0.2
 set system static-host-mapping host-name ragnar.zinn.tech alias atlas.zinn.tech
 set system static-host-mapping host-name ares.zinn.tech inet 10.10.0.4
-set system static-host-mapping host-name ares.zinn.tech alias ares.zinn.ca
-set system static-host-mapping host-name pikvm.zinn.tech inet 10.10.0.5
-set system static-host-mapping host-name pikvm.zinn.tech alias pikvm.zinn.ca
