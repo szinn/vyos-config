e7d9e42411b9f688745dd2bdabbe522b2c6b9fa3
Scotte Zinn <scotte@zinn.ca>
2023-05-01 14:25:13 -0400

    Change cni-services to pod-services

--- config-parts/firewall-zone.sh
+++ config-parts/firewall-zone.sh
@@ -38,7 +38,7 @@ set firewall zone services from trusted firewall name 'trusted-services'
 set firewall zone services from guest firewall name 'guest-services'
 set firewall zone services from iot firewall name 'iot-services'
 set firewall zone services from wan firewall name 'wan-services'
-set firewall zone services interface 'cni-services'
+set firewall zone services interface 'pod-services'
 
 set firewall zone servers description 'Servers VLAN'
 set firewall zone servers default-action 'drop'
