bf87c51a892cd80b88cd0360f21fdf53b7d67fea
Scotte Zinn <scotte@zinn.ca>
2024-01-02 19:20:49 -0500

    Base protocols

--- config-parts/0-ipv4-firewall-functions.sh
+++ config-parts/0-ipv4-firewall-functions.sh
@@ -48,3 +48,84 @@ function drop-traffic {
     drop-traffic-${type} ${ZONE}
   done
 }
+
+function allow-traffic-ntp {
+  zone=$1
+
+  # set firewall ipv4 name $zone rule 100 description 'Rule: accept ntp'
+  set firewall ipv4 name $zone rule 100 action 'accept'
+  set firewall ipv4 name $zone rule 100 destination port 'ntp'
+  set firewall ipv4 name $zone rule 100 protocol 'udp'
+}
+
+function allow-traffic-dhcp {
+  zone=$1
+
+  # set firewall ipv4 name $zone rule 110 description 'Rule: accept dhcp'
+  set firewall ipv4 name $zone rule 110 action 'accept'
+  set firewall ipv4 name $zone rule 110 source port 'bootps,bootpc'
+  set firewall ipv4 name $zone rule 110 destination port 'bootps,bootpc'
+  set firewall ipv4 name $zone rule 110 protocol 'udp'
+}
+
+function allow-traffic-igmp {
+  zone=$1
+
+  # set firewall ipv4 name $zone rule 120 description 'Rule: accept igmp'
+  set firewall ipv4 name $zone rule 120 action 'accept'
+  set firewall ipv4 name $zone rule 120 protocol '2'
+}
+
+function allow-traffic-mdns {
+  zone=$1
+
+  # set firewall ipv4 name $zone rule 130 description 'Rule: accept mdns'
+  set firewall ipv4 name $zone rule 130 action 'accept'
+  set firewall ipv4 name $zone rule 130 source port 'mdns'
+  set firewall ipv4 name $zone rule 130 destination port 'mdns'
+  set firewall ipv4 name $zone rule 130 protocol 'udp'
+}
+
+function allow-traffic-icmp {
+  zone=$1
+
+  # set firewall ipv4 name $zone rule 140 description 'Rule: accept icmp'
+  set firewall ipv4 name $zone rule 140 action 'accept'
+  set firewall ipv4 name $zone rule 140 protocol 'icmp'
+}
+
+function allow-traffic-bgp {
+  zone=$1
+
+  # set firewall ipv4 name $zone rule 150 description 'Rule: accept bgp'
+  set firewall ipv4 name $zone rule 150 action 'accept'
+  set firewall ipv4 name $zone rule 150 destination port 'bgp'
+  set firewall ipv4 name $zone rule 150 protocol 'tcp'
+}
+
+function allow-traffic-ssh {
+  zone=$1
+
+  # set firewall ipv4 name $zone rule 160 description 'Rule: accept ssh'
+  set firewall ipv4 name $zone rule 160 action 'accept'
+  set firewall ipv4 name $zone rule 160 destination port 'ssh'
+  set firewall ipv4 name $zone rule 160 protocol 'tcp'
+}
+
+function allow-traffic-dns {
+  zone=$1
+
+  # set firewall ipv4 name $zone rule 170 description 'Rule: accept dns'
+  set firewall ipv4 name $zone rule 170 action 'accept'
+  set firewall ipv4 name $zone rule 170 destination port 'domain,domain-s'
+  set firewall ipv4 name $zone rule 170 protocol 'tcp_udp'
+}
+
+function allow-traffic-wireguard {
+  zone=$1
+
+  # set firewall ipv4 name $zone rule 180 description 'Rule: accept wireguard'
+  set firewall ipv4 name $zone rule 180 action 'accept'
+  set firewall ipv4 name $zone rule 180 destination port '51820'
+  set firewall ipv4 name $zone rule 180 protocol 'udp'
+}
