bafec93a520fa6dadf1e550453b32a9c50091521
repo-jeeves <106431701+repo-jeeves[bot]@users.noreply.github.com>
2024-03-16 09:12:51 +0000

    feat(docker-image): update ghcr.io/twin/gatus docker tag to v5.8.0
    | datasource | package            | from   | to     |
    | ---------- | ------------------ | ------ | ------ |
    | docker     | ghcr.io/twin/gatus | v5.7.0 | v5.8.0 |

--- config-parts/containers.sh
+++ config-parts/containers.sh
@@ -59,7 +59,7 @@ set container name dnsdist volume config mode 'ro'
 set container name gatus cap-add 'net-bind-service'
 set container name gatus cap-add 'net-raw'
 set container name gatus environment DISCORD_WEBHOOK_URL value ${DISCORD_WEBHOOK_URL}
-set container name gatus image 'ghcr.io/twin/gatus:v5.7.0'
+set container name gatus image 'ghcr.io/twin/gatus:v5.8.0'
 set container name gatus memory '0'
 set container name gatus network services address '10.0.5.5'
 set container name gatus shared-memory '0'
