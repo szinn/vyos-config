c31d87d6d4517e000b119914930d8aa7d86b40e9
Scotte Zinn <scotte@zinn.ca>
2024-04-21 15:18:19 -0400

    feat: turn down bgp

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -1,43 +1,43 @@
 #!/bin/vbash
 
 # BGP configuration
-set protocols bgp neighbor 10.11.0.16 description 'k8s-1'
-set protocols bgp neighbor 10.11.0.16 address-family ipv4-unicast
-set protocols bgp neighbor 10.11.0.16 remote-as '65001'
-set protocols bgp neighbor 10.11.0.17 description 'k8s-2'
-set protocols bgp neighbor 10.11.0.17 address-family ipv4-unicast
-set protocols bgp neighbor 10.11.0.17 remote-as '65001'
-set protocols bgp neighbor 10.11.0.18 description 'k8s-3'
-set protocols bgp neighbor 10.11.0.18 address-family ipv4-unicast
-set protocols bgp neighbor 10.11.0.18 remote-as '65001'
-set protocols bgp neighbor 10.11.0.19 description 'k8s-4'
-set protocols bgp neighbor 10.11.0.19 address-family ipv4-unicast
-set protocols bgp neighbor 10.11.0.19 remote-as '65001'
-set protocols bgp neighbor 10.11.0.20 description 'k8s-5'
-set protocols bgp neighbor 10.11.0.20 address-family ipv4-unicast
-set protocols bgp neighbor 10.11.0.20 remote-as '65001'
-set protocols bgp neighbor 10.11.0.21 description 'k8s-6'
-set protocols bgp neighbor 10.11.0.21 address-family ipv4-unicast
-set protocols bgp neighbor 10.11.0.21 remote-as '65001'
+# set protocols bgp neighbor 10.11.0.16 description 'k8s-1'
+# set protocols bgp neighbor 10.11.0.16 address-family ipv4-unicast
+# set protocols bgp neighbor 10.11.0.16 remote-as '65001'
+# set protocols bgp neighbor 10.11.0.17 description 'k8s-2'
+# set protocols bgp neighbor 10.11.0.17 address-family ipv4-unicast
+# set protocols bgp neighbor 10.11.0.17 remote-as '65001'
+# set protocols bgp neighbor 10.11.0.18 description 'k8s-3'
+# set protocols bgp neighbor 10.11.0.18 address-family ipv4-unicast
+# set protocols bgp neighbor 10.11.0.18 remote-as '65001'
+# set protocols bgp neighbor 10.11.0.19 description 'k8s-4'
+# set protocols bgp neighbor 10.11.0.19 address-family ipv4-unicast
+# set protocols bgp neighbor 10.11.0.19 remote-as '65001'
+# set protocols bgp neighbor 10.11.0.20 description 'k8s-5'
+# set protocols bgp neighbor 10.11.0.20 address-family ipv4-unicast
+# set protocols bgp neighbor 10.11.0.20 remote-as '65001'
+# set protocols bgp neighbor 10.11.0.21 description 'k8s-6'
+# set protocols bgp neighbor 10.11.0.21 address-family ipv4-unicast
+# set protocols bgp neighbor 10.11.0.21 remote-as '65001'
 
-set protocols bgp neighbor 10.12.0.16 description 'stage-1'
-set protocols bgp neighbor 10.12.0.16 address-family ipv4-unicast
-set protocols bgp neighbor 10.12.0.16 remote-as '65002'
-set protocols bgp neighbor 10.12.0.17 description 'stage-2'
-set protocols bgp neighbor 10.12.0.17 address-family ipv4-unicast
-set protocols bgp neighbor 10.12.0.17 remote-as '65002'
-set protocols bgp neighbor 10.12.0.18 description 'stage-3'
-set protocols bgp neighbor 10.12.0.18 address-family ipv4-unicast
-set protocols bgp neighbor 10.12.0.18 remote-as '65002'
-set protocols bgp neighbor 10.12.0.19 description 'stage-4'
-set protocols bgp neighbor 10.12.0.19 address-family ipv4-unicast
-set protocols bgp neighbor 10.12.0.19 remote-as '65002'
-set protocols bgp neighbor 10.12.0.20 description 'stage-5'
-set protocols bgp neighbor 10.12.0.20 address-family ipv4-unicast
-set protocols bgp neighbor 10.12.0.20 remote-as '65002'
-set protocols bgp neighbor 10.12.0.21 description 'stage-6'
-set protocols bgp neighbor 10.12.0.21 address-family ipv4-unicast
-set protocols bgp neighbor 10.12.0.21 remote-as '65002'
+# set protocols bgp neighbor 10.12.0.16 description 'stage-1'
+# set protocols bgp neighbor 10.12.0.16 address-family ipv4-unicast
+# set protocols bgp neighbor 10.12.0.16 remote-as '65002'
+# set protocols bgp neighbor 10.12.0.17 description 'stage-2'
+# set protocols bgp neighbor 10.12.0.17 address-family ipv4-unicast
+# set protocols bgp neighbor 10.12.0.17 remote-as '65002'
+# set protocols bgp neighbor 10.12.0.18 description 'stage-3'
+# set protocols bgp neighbor 10.12.0.18 address-family ipv4-unicast
+# set protocols bgp neighbor 10.12.0.18 remote-as '65002'
+# set protocols bgp neighbor 10.12.0.19 description 'stage-4'
+# set protocols bgp neighbor 10.12.0.19 address-family ipv4-unicast
+# set protocols bgp neighbor 10.12.0.19 remote-as '65002'
+# set protocols bgp neighbor 10.12.0.20 description 'stage-5'
+# set protocols bgp neighbor 10.12.0.20 address-family ipv4-unicast
+# set protocols bgp neighbor 10.12.0.20 remote-as '65002'
+# set protocols bgp neighbor 10.12.0.21 description 'stage-6'
+# set protocols bgp neighbor 10.12.0.21 address-family ipv4-unicast
+# set protocols bgp neighbor 10.12.0.21 remote-as '65002'
 
-set protocols bgp parameters router-id '10.0.0.1'
-set protocols bgp system-as '65000'
+# set protocols bgp parameters router-id '10.0.0.1'
+# set protocols bgp system-as '65000'
